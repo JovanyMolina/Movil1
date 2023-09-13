71dbb438dd4351cf73cbf129020ef1a5127c70c6
JovanyMolina <s20120208@alumnos.itsur.edu.mx>
2023-09-12 21:03:39 -0600

    Practica Calcula una propina personalizada

--- propina/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ propina/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -36,15 +36,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiptime.ui.theme.TipTimeTheme
 import java.text.NumberFormat
-import androidx.compose.material3.TextField
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Switch
+
 
 
 class MainActivity : ComponentActivity() {
@@ -68,7 +71,13 @@ class MainActivity : ComponentActivity() {
 fun TipTimeLayout() {
     var amountInput by remember { mutableStateOf("") }
     val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+
+    var amountInputdos by remember { mutableStateOf("") }
+    val amountdos = amountInputdos.toDoubleOrNull() ?: 0.0
+
+    var roundUp by remember { mutableStateOf(false) }
+
+    val tip = calculateTip(amount,amountdos,roundUp)
 
     Column(
         modifier = Modifier.padding(40.dp),
@@ -95,10 +104,24 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp)
                 .align(alignment = Alignment.Start)
         )
+        EditNumberFieldDos(
+            value = amountInputdos,
+            onValueChange = { amountInputdos = it },
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        )
+        RoundTheTipRow(
+            roundUp = roundUp,
+            onRoundUpChanged = { roundUp = it },
+            modifier = Modifier.padding(bottom = 32.dp)
+        )
         Text(
             text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
+
+
 
         Spacer(modifier = Modifier.height(150.dp))
     }
@@ -112,17 +135,12 @@ fun TipTimeLayout() {
  * according to the local currency.
  * Example would be "$10.00".
  *
- *  TextField(
- *         value = value,
- *         onValueChange = onValueChange,
- *         singleLine = true,
- *         label = { Text(stringResource(R.string.Propina)) },
- *         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
- *         modifier = modifier
- *     )
  */
-private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
-    val tip = tipPercent / 100 * amount
+private fun calculateTip(amount: Double, amountdos: Double, roundUp: Boolean ): String {
+    var tip = amountdos / 100 * amount
+    if (roundUp) {
+        tip = kotlin.math.ceil(tip)
+    }
     return NumberFormat.getCurrencyInstance().format(tip)
 }
 
@@ -147,6 +165,45 @@ fun EditNumberField(
         label = { Text(stringResource(R.string.bill_amount)) },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         modifier = modifier
+    )
+
+}
+@Composable
+fun EditNumberFieldDos(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        singleLine = true,
+        label = { Text(stringResource(R.string.Propina)) },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = modifier
+    )
+}
+@Composable
+fun RoundTheTipRow(
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .size(48.dp),
+        verticalAlignment = Alignment.CenterVertically
+
+    )
+    {
+        Text(text = stringResource(R.string.round_up_tip))
+    }
+    Switch(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.End),
+        checked = roundUp,
+        onCheckedChange = onRoundUpChanged,
     )
 
 }
