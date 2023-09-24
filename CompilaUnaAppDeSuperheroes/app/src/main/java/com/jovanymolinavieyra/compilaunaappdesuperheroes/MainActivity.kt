93975c46fc89252ee40c974e8cd829563c59c77e
JovanyMolina <s20120208@alumnos.itsur.edu.mx>
2023-09-24 14:08:51 -0600

    ajustes con el superhero

--- CompilaUnaAppDeSuperheroes/app/src/main/java/com/jovanymolinavieyra/compilaunaappdesuperheroes/MainActivity.kt
+++ CompilaUnaAppDeSuperheroes/app/src/main/java/com/jovanymolinavieyra/compilaunaappdesuperheroes/MainActivity.kt
@@ -4,43 +4,72 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.jovanymolinavieyra.compilaunaappdesuperheroes.ui.theme.CompilaUnaAppDeSuperheroesTheme
+import com.jovanymolinavieyra.compilaunaappdesuperheroes.model.HeroDataSource
+import com.jovanymolinavieyra.compilaunaappdesuperheroes.ui.theme.SuperheroesTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CompilaUnaAppDeSuperheroesTheme {
+            SuperheroesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SuperheroesApp()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CompilaUnaAppDeSuperheroesTheme {
-        Greeting("Android")
+    @Composable
+    fun SuperheroesApp() {
+        Scaffold {
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar()
+            }
+        }
+
+    }
+
+    /**
+     * Composable that displays a Top Bar with an icon and text.
+     *
+     * @param modifier modifiers to set to this composable
+     */
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun TopAppBar(modifier: Modifier = Modifier) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge,
+                )
+            },
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun SuperHeroesPreview() {
+        SuperheroesTheme {
+            SuperheroesApp()
+        }
     }
 }
